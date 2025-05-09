3eaeb0c8b6e513672765c7a96ec55fd6030f38c0
YeomYuJun <yyj2046@gmail.com>
2025-05-09 21:53:29 +0900

    Add Minimumpoint data type

---
+++ src/main/java/com/yy/allgomath/datatype/MinimumPoint.java
@@ -0,0 +1,12 @@
+package com.yy.allgomath.datatype;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MinimumPoint {
+    private double x;
+    private double y;
+    private double value;
+}
