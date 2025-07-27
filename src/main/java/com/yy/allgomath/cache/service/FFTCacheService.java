aeb66b005a48f3a0099a1c7cbcf7412292a696fc
YeomYuJun <yyj2046@gmail.com>
2025-07-27 14:06:42 +0900

    feat add FFT caching logic

---
+++ src/main/java/com/yy/allgomath/cache/service/FFTCacheService.java
@@ -0,0 +1,20 @@
+package com.yy.allgomath.cache.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FFTCacheService {
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    public Object get(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public void set(String key, Object value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+}
