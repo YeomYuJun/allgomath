c1dfc913f88dbe405b02fa005ef30a3c5d3eddb2
YeomYuJun <yyj2046@gmail.com>
2025-08-22 00:52:55 +0900

    fix add redis bean config

--- src/main/java/com/yy/allgomath/config/RedisConfig.java
+++ src/main/java/com/yy/allgomath/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.yy.allgomath.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -7,9 +8,16 @@ import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
+
+    @Value("${spring.data.redis.host:localhost}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port:6379}")
+    private int redisPort;
+
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        return new LettuceConnectionFactory(redisHost, redisPort);
     }
 
     @Bean
