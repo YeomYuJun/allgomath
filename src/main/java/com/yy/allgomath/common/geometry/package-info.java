d91e7157cf210779b47e07e235d174e7cb5a6a92
YeomYuJun <yyj2046@gmail.com>
2026-06-14 00:48:06 +0900

    chore: add common math/geometry package skeletons

---
+++ src/main/java/com/yy/allgomath/common/geometry/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * 공통 기하 값객체 (Point2D, Point3D, BoundingBox 등).
+ * SP-1에서는 골격만 생성하며, 실제 VO 이동/단일화는 해당 VO를 사용하는 도메인(optimization/math) SP에서 수행한다.
+ */
+package com.yy.allgomath.common.geometry;
---
+++ src/main/java/com/yy/allgomath/common/math/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * 공통 수학 값객체/유틸 (Complex, MathConstants 등).
+ * SP-1에서는 골격만 생성하며, 실제 VO 이동은 해당 VO를 사용하는 도메인(signal 등) SP에서 수행한다.
+ */
+package com.yy.allgomath.common.math;
